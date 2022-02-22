a5a5be70868ec49bc74fd8732efc1538391975e5
sundaramyadav <sundaramy46@gmail.com>
2022-02-22 14:46:34 -0500

    Props and spread operator

--- React/employee-details/src/components/EmployeeDetailsComponent.js
+++ React/employee-details/src/components/EmployeeDetailsComponent.js
@@ -1,20 +1,29 @@
-export default function EmployeeDetailsComponent() {
+export default function EmployeeDetailsComponent(props) {
 
-    var details = {
-        name: "Sundaram",
-        avatar: "https://www.insureon.com/-/media/blog/posts/2021/photo_group-of-employees-working-on-project.png?h=370&iar=0&w=750&rev=3faabd3c0f7c4e11966caaa037fa4fc8",
-        createdAt: "Today",
-        id: 1
-    }
+    // var details = {
+    //     name: "Sundaram",
+    //     avatar: "https://www.insureon.com/-/media/blog/posts/2021/photo_group-of-employees-working-on-project.png?h=370&iar=0&w=750&rev=3faabd3c0f7c4e11966caaa037fa4fc8",
+    //     createdAt: "Today",
+    //     id: 1
+    // }
 
     return (
-        <div>
-            <img src={details.avatar} alt={details.name} />
-            <h1>User Name is: {details.name}</h1>
-            <p>
-                <span>The User Selected has the Following Id: {details.id}.</span>
-                <span>The User was created On: {details.createdAt}</span>
+      
+            
+            <div class="card" style={{width: "18rem", margin: "50px"}}>
+            <img src={props.avatar} className="card-img-top" alt={props.name}/>
+            <div className="card-body">
+            <h5 className="card-title">{props.name}</h5>
+            <p className="card-text">
+               <b> Id: </b>{props.id}<br></br>
+               <b>Joined At: </b>{props.createdAt}
             </p>
-        </div>
+            <input type="button" class="btn btn-primary" value="More details"/>
+            
+            </div>
+            </div>
+
+
+        
     )
 }
--- React/employee-details/src/index.js
+++ React/employee-details/src/index.js
@@ -43,11 +43,17 @@ import ReactDOM from 'react-dom';
 // ReactDOM.render(<App></App>, document.getElementById("root"))
 
 
-// import App from "./components/EmployeeDetailsComponent";
-// ReactDOM.render(<App></App>, document.getElementById("root"))
+import App from "./components/EmployeeDetailsComponent";
+var details = {
+    name: "Sundaram",
+    avatar: "https://www.insureon.com/-/media/blog/posts/2021/photo_group-of-employees-working-on-project.png?h=370&iar=0&w=750&rev=3faabd3c0f7c4e11966caaa037fa4fc8",
+    createdAt: "Today",
+    id: 1
+}
+ReactDOM.render(<App {...details}></App>, document.getElementById("root"))
 
-import App from "./components/DataBindingComponent";
-ReactDOM.render(<App></App>, document.getElementById("root"))
+// import App from "./components/DataBindingComponent";
+// ReactDOM.render(<App></App>, document.getElementById("root"))
 
 // import App from "./classComponent/IncrementCounter";
 // ReactDOM.render(<App></App>, document.getElementById("root"))
