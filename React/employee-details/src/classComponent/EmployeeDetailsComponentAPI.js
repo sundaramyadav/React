78e2f8778550f4d8077d7bf7f80a05ab9c21b823
sundaramyadav <sundaramy46@gmail.com>
2022-02-23 14:44:29 -0500

    Delete is done

--- React/employee-details/src/classComponent/EmployeeDetailsComponentAPI.js
+++ React/employee-details/src/classComponent/EmployeeDetailsComponentAPI.js
@@ -15,13 +15,45 @@ export default class EmployeeListComponent extends React.Component {
             <div>
                 <h1>Employee List is given Below:</h1>
                 {this.state.employeeList.map((employee) => {
-                    return <EmployeeDetailsComponent key={employee.id} {...employee}></EmployeeDetailsComponent>
+                    return <EmployeeDetailsComponent key={employee.AccountNumber} deleteEmployee={this.deleteEmployee} {...employee}></EmployeeDetailsComponent>
                 })}
             </div>
         )
     }
 
+    // deleteEmployee = (event) => {
+    //     debugger;
+    //     alert(event.target.accountNumber);
+    //     Axios.delete("https://localhost:5001/api/SundaramSbaccount/DeleteAcc?id=" +event.target.AccountNumber).then(() => {
+    //         alert("Employee is Deleted");
+    //         this.getData();
+    //     })
+    // }
+    // componentDidMount() {
+    //     var dataPromise = Axios.get("https://localhost:5001/api/SundaramSbaccount/Allacc");
+    //     dataPromise.then((response) => {
+    //         this.setState({
+    //             employeeList: response.data
+    //         })
+    //     })
+    // }
+
+
+
+    deleteEmployee = (event) => {
+        debugger;
+       
+        Axios.delete("https://localhost:5001/api/SundaramSbaccount/DeleteAcc?id=" +event.target.id).then(() => {
+            alert("Employee is Deleted");
+            this.getData();
+        })
+    }
+
     componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
         var dataPromise = Axios.get("https://localhost:5001/api/SundaramSbaccount/Allacc");
         dataPromise.then((response) => {
             this.setState({
