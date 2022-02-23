c5f44d8fb6e32638807c7edd1df54c29d468ed28
sundaramyadav <sundaramy46@gmail.com>
2022-02-23 14:45:14 -0500

    delete is done

--- React/employee-details/src/components/ApiSbaccountdetails.js
+++ React/employee-details/src/components/ApiSbaccountdetails.js
@@ -1,11 +1,6 @@
 export default function EmployeeDetailsComponent(props) {
     debugger;
-        // var details = {
-        //     name: "Sundaram",
-        //     avatar: "https://www.insureon.com/-/media/blog/posts/2021/photo_group-of-employees-working-on-project.png?h=370&iar=0&w=750&rev=3faabd3c0f7c4e11966caaa037fa4fc8",
-        //     createdAt: "Today",
-        //     id: 1
-        // }
+        
     
         return (
           
@@ -19,7 +14,7 @@ export default function EmployeeDetailsComponent(props) {
                    <b> currentBalance: </b>{props.currentBalance}<br></br>
                    <b>Address: </b>{props.customerAddress}
                 </p>
-                <input type="button" class="btn btn-primary" value="More details"/>
+                <input type="button" id={props.accountNumber} class="btn btn-primary" value="Delete" onClick={props.deleteEmployee} />
                 
                 </div>
                 </div>
